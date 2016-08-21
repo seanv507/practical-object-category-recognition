3a4818640924e65984ac53fa66a595cffab6d578
seanv507 <sean.violante@gmail.com>
2016-08-21 17:13:38 +0200

    updated file http. still not working

--- extra/download.sh
+++ extra/download.sh
@@ -9,7 +9,7 @@ if test ! -d data/tmp/VOCdevkit
 then
     (
         cd data/tmp
-        wget -c -nc http://pascallin.ecs.soton.ac.uk/challenges/VOC/voc2007/VOCtrainval_06-Nov-2007.tar
+        wget -c -nc http://host.robots.ox.ac.uk/pascal/VOC/voc2007/VOCtrainval_06-Nov-2007.tar
         tar xzvf VOCtrainval_06-Nov-2007.tar
     )
 fi
